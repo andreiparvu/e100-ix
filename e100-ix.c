955ecabacb0b1802b8aaf782aa4919bd4ee8ebb4
Andrei Parvu <andrei.prv@gmail.com>
2014-03-26 21:47:45 +0200

    Final version

--- e100-ix.c
+++ e100-ix.c
@@ -25,16 +25,25 @@ MODULE_LICENSE("GPL");
 #define CMD_BASE 0x02
 #define SCB_POINTER_BASE 0x04
 
-#define CUC_START (0b0001 << 4) | 0xbc00
-#define CUC_RESUME (0b0010 << 4) | 0xbc00
+#define CUC_START (0b0001 << 4) | 0xbc00 // cmd start + interuperi doar pt
+					 // receive
+#define CUC_RESUME (0b0010 << 4) | 0xbc00 // cmd resume + intreruperi doar pt
+					  // receive
 #define CUC_LOAD_BASE (0b0110 << 4)
 
 #define RUC_START 0b0001
 #define RUC_RESUME 0b0010
 #define RUC_LOAD_BASE 0b0110
 
+#define CB_SEND 0x4004 // send + suspend bit
+#define S_BIT (1 << 14)
+
+#define FR_BIT (1 << 14)
+
 #define TRUE 1
 #define FALSE 0
+
+#define MAX_ETHERNET_LENGTH 1518
 
 // CB structure
 struct e100_cb {
@@ -47,7 +56,7 @@ struct e100_cb {
 	u8 threshold;
 	u8 tbd_count;
 
-	uint8_t data[1518];
+	uint8_t data[MAX_ETHERNET_LENGTH];
 
 	dma_addr_t dma_addr;
 	struct e100_cb *next, *prev;
@@ -65,14 +74,18 @@ struct e100_data {
 	struct e100_cb *cur_rx_cb;
 
 	int has_started;
-
 };
+
+DEFINE_SPINLOCK(xmit_lock);
+
+#define CMD_TIMEOUT 20000
+#define SMALL_TIMEOUT 20
 
 // Set a command to the command unit
 void set_cmd(unsigned long base, unsigned short value, int no_wait) {
 	int i;
 
-	for (i = 0; i < 20000; i++) {
+	for (i = 0; i < CMD_TIMEOUT; i++) {
 		unsigned char v = inw(base);
 
 		// Wait for an empty register - it finished processing the last
@@ -83,7 +96,7 @@ void set_cmd(unsigned long base, unsigned short value, int no_wait) {
 			break;
 		}
 
-		if (i > 20) {
+		if (i > SMALL_TIMEOUT) {
 			udelay(5);
 		}
 	}
@@ -93,26 +106,34 @@ irqreturn_t e100_irq(int irq_no, void *dev_id)
 {
 	struct e100_data *data = (struct e100_data*)dev_id;
 	struct sk_buff *skb;
+	unsigned short status = inw(data->net_dev->base_addr);
 
-	//printk(LOG_LEVEL "Nasol %d %d\n", data->cur_rx_cb->status,
-	//		data->cur_rx_cb->tcb_byte_count);
+	if ((status & FR_BIT) > 0) { // if receive
+		// Alloc a new skb structure
+		skb = dev_alloc_skb(data->cur_rx_cb->tcb_byte_count + 2);
 
-	// Alloc a new skb structure
-	skb = dev_alloc_skb(data->cur_rx_cb->tcb_byte_count + 2);
+		// Copy the data from the cb to the skb structure
+		memcpy(skb_put(skb, data->cur_rx_cb->tcb_byte_count),
+				data->cur_rx_cb->data,
+				data->cur_rx_cb->tcb_byte_count);
 
-	memcpy(skb_put(skb, data->cur_rx_cb->tcb_byte_count),
-			data->cur_rx_cb->data, data->cur_rx_cb->tcb_byte_count);
+		skb->dev = data->net_dev;
+		skb->protocol = eth_type_trans(skb, data->net_dev);
+		skb->ip_summed = CHECKSUM_UNNECESSARY;
 
-	skb->dev = data->net_dev;
-	skb->protocol = eth_type_trans(skb, data->net_dev);
-	skb->ip_summed = CHECKSUM_UNNECESSARY;
+		netif_rx(skb);
 
-	netif_rx(skb);
+		// advance to next cb
+		data->cur_rx_cb = data->cur_rx_cb->next;
 
-	data->cur_rx_cb = data->cur_rx_cb->next;
-
-	set_cmd(data->net_dev->base_addr, 0xFF00, TRUE);
-	set_cmd(data->net_dev->base_addr + CMD_BASE, RUC_RESUME, FALSE);
+		// signal that interrupt was handled
+		set_cmd(data->net_dev->base_addr, 0xFF00, TRUE);
+		// resume reception
+		set_cmd(data->net_dev->base_addr + CMD_BASE, RUC_RESUME, FALSE);
+	} else {
+		// signal that interrupt was handled
+		set_cmd(data->net_dev->base_addr, 0xFF00, TRUE);
+	}
 
 	return IRQ_HANDLED;
 }
@@ -122,9 +143,11 @@ int e100_open(struct net_device *net_dev)
 	struct e100_data *data = netdev_priv(net_dev);
 	int ret;
 
-	return 0;
+	// assign random mac
+	net_dev->addr_assign_type = NET_ADDR_RANDOM;
+	eth_random_addr(net_dev->dev_addr);
 
-	printk(LOG_LEVEL "in open");
+	// base address for control block
 	net_dev->base_addr = data->pdev->resource[1].start;
 
 	outw(0, net_dev->base_addr + CMD_BASE);
@@ -132,17 +155,14 @@ int e100_open(struct net_device *net_dev)
 	// Load address of first CB
 	data->cur_tx_cb = data->tx_cbl[0];
 	data->cur_rx_cb = data->rx_cbl[0];
-	//printk(LOG_LEVEL "%lu\n", data->cur_cb->dma_addr);
 
 	outl(cpu_to_le32(data->cur_rx_cb->dma_addr), net_dev->base_addr +
 			SCB_POINTER_BASE);
 
 	set_cmd(net_dev->base_addr + CMD_BASE, RUC_START, FALSE);
 
-	data->has_started = 0;
+	data->has_started = FALSE;
 	ret = request_irq(net_dev->irq, e100_irq, IRQF_SHARED, "e100", data);
-
-	printk(LOG_LEVEL "%d %d\n", ret, net_dev->irq);
 
 	return 0;
 }
@@ -150,30 +170,33 @@ int e100_open(struct net_device *net_dev)
 netdev_tx_t e100_start_xmit(struct sk_buff *skb, struct net_device *net_dev)
 {
 	struct e100_data *data = netdev_priv(net_dev);
+	struct e100_cb *cur_cb;
+	int has_started;
 
-	return 0;
+	spin_lock(&xmit_lock);
+	cur_cb = data->cur_tx_cb;
 
-	printk(LOG_LEVEL "sk buff de dimensiune %d\n", skb->len);
+	// mark previous cb suspend bit as false
+	cur_cb->prev->cmd &= ~S_BIT;
+	// mark send for current cb
+	cur_cb->cmd = cpu_to_le16(CB_SEND | S_BIT);
+	has_started = data->has_started;
+	// move current cb
+	data->cur_tx_cb = data->cur_tx_cb->next;
+	spin_unlock(&xmit_lock);
 
-	memcpy(data->cur_tx_cb->data, skb->data, skb->len);
-	data->cur_tx_cb->tcb_byte_count = skb->len;
+	memcpy(cur_cb->data, skb->data, skb->len);
+	cur_cb->tcb_byte_count = skb->len;
 
-	data->cur_tx_cb->prev->cmd &= ~(1 << 14);
-	data->cur_tx_cb->cmd = cpu_to_le16(0x4004); // bitul S setat si bitul si
-						// cmd cu valoarea 0b100
-	wmb();
-
-	if (data->has_started == 0) {
-		data->has_started = 1;
-		printk(LOG_LEVEL "Has started\n");
-		outl(cpu_to_le32(data->cur_tx_cb->dma_addr), net_dev->base_addr +
-			SCB_POINTER_BASE);
+	if (has_started == FALSE) {
+		data->has_started = TRUE;
+		outl(cpu_to_le32(cur_cb->dma_addr), net_dev->base_addr +
+				SCB_POINTER_BASE);
 		set_cmd(net_dev->base_addr + CMD_BASE, CUC_START, FALSE);
 	} else {
 		set_cmd(net_dev->base_addr + CMD_BASE, CUC_RESUME, FALSE);
 	}
 
-	data->cur_tx_cb = data->cur_tx_cb->next;
 	return NETDEV_TX_OK;
 }
 
@@ -181,6 +204,7 @@ int e100_stop(struct net_device *net_dev)
 {
 	struct e100_data *data = netdev_priv(net_dev);
 
+	netif_stop_queue(net_dev);
 	free_irq(net_dev->irq, data);
 
 	return 0;
@@ -211,6 +235,7 @@ static void alloc_cbl(struct e100_cb *cbl[MAX_CBS], struct pci_dev *dev) {
 	for (i = 0; i < MAX_CBS; i++) {
 		dma_addr_t dma_addr;
 
+		// alloc structure for cb
 		cbl[i] = pci_alloc_consistent(dev,
 				sizeof(struct e100_cb), &dma_addr);
 		cbl[i]->dma_addr = dma_addr;
@@ -219,6 +244,7 @@ static void alloc_cbl(struct e100_cb *cbl[MAX_CBS], struct pci_dev *dev) {
 		cbl[i]->tbd_count = 0;
 		cbl[i]->cmd = 0;
 
+		// make the links
 		if (i > 0) {
 			cbl[i - 1]->link = cpu_to_le32(dma_addr);
 			cbl[i - 1]->next = cbl[i];
@@ -252,7 +278,7 @@ int e100_probe(struct pci_dev *dev, const struct pci_device_id *id)
 	ret = pci_enable_device(dev);
 
 	if (ret < 0) {
-		return -1;
+		return ret;
 	}
 
 	pci_set_master(dev);
@@ -262,14 +288,17 @@ int e100_probe(struct pci_dev *dev, const struct pci_device_id *id)
 
 	strcpy(net_dev->name, "ixeth%d");
 	net_dev->netdev_ops = &e100_ops;
+
 	ret = register_netdev(net_dev);
+
+	if (ret < 0) {
+		return ret;
+	}
 
 	net_dev->irq = dev->irq;
 
 	alloc_cbl(data->tx_cbl, dev);
 	alloc_cbl(data->rx_cbl, dev);
-
-	printk(LOG_LEVEL "done\n");
 
 	return 0;
 }
@@ -309,9 +338,7 @@ static int e100_init(void)
 {
 	int ret = pci_register_driver(&e100_driver);
 
-	printk(LOG_LEVEL "%d\n", ret);
-
-	return 0;
+	return ret;
 }
 
 static void e100_exit(void)
--- script.sh
+++ script.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 ifconfig ixeth0 up
-ifconfig ixeth0 172.20.0.2 netmask 255.255.255.0 broadcast 0.0.0.0 hw ether 52:54:00:12:34:56
+ifconfig ixeth0 172.30.0.2 netmask 255.255.255.0 broadcast 0.0.0.0 hw ether 52:54:00:12:34:56
 
 ifconfig ixeth1 up
 ifconfig ixeth1 172.20.0.3 netmask 255.255.255.0 broadcast 0.0.0.0 hw ether 52:54:00:12:34:56
